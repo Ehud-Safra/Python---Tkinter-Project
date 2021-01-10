b5c9ae7d7393c4a69643d98f73330d65d227dad1
Ehud-Safra <75996234+Ehud-Safra@users.noreply.github.com>
2021-01-10 17:17:08 +0200

    Update simpleloginform4.py

--- simpleloginform4.py
+++ simpleloginform4.py
@@ -1,38 +1,39 @@
 from tkinter import *
 import tkinter.messagebox
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://mail.google.com/mail/u/1/#inbox'
+page = requests.get(URL)
 
 
 
 win = Tk()
-win.title("Login 4")
-win.geometry("400x250")
+win.title("Scrap Me")
+win.geometry("400x300")
 
 # define click function for login button
 def myclick():
 
-    greeting_user = "Hello " + user_entry.get()
+    greeting_user = "Hello " + search_entry.get()
     my_label = Label(win, text=greeting_user)
     my_label.place(relx=0.5, rely=0.4, anchor=CENTER)
 
     
 
-# make user name label
-user_label = Label(win, text="User Name")
-user_label.grid(column=0, row=0)
-# make user name input box
-user_entry = Entry(win, width=10)
-user_entry.grid(column=1, row=0)
+# make search label
+search_label = Label(win, text="Search Text")
+search_label.place(relx=0.5, anchor = N)
 
-# make password label
-password_label = Label(win, text="Password")
-password_label.grid(column=0, row=1, sticky=W)
-# make password input box
-password_entry = Entry(win, width=10)
-password_entry.grid(column=1, row=1)
+# make text input box
+search_entry = Entry(win, width=50)
+search_entry.place(relx=0.5, rely=0.1, anchor=CENTER)
+
+
 
 # make login button and place it in the window
-logIn_Button = Button(win, text="Login", command=myclick)
-logIn_Button.place(relx=0.5, rely=0.5, anchor=CENTER)
+search_Button = Button(win, text="Search", command=myclick)
+search_Button.pack(side= BOTTOM)
 
 
 win.mainloop()
